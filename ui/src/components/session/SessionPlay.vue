a2f2028bcdb9268113f24b51a2223489a51e399c
vagnersaller <vsaller@yahoo.com>
2020-07-06 16:46:35 -0300

    [wip] ui: tests to control time slider

--- ui/src/components/session/SessionPlay.vue
+++ ui/src/components/session/SessionPlay.vue
@@ -54,6 +54,12 @@
                 class="pt-4"
                 tile
               >
+                <v-card-text
+                  v-if="sliderChange"
+                >
+                  {{ currentTime }}
+                </v-card-text>
+
                 <v-icon
                   v-if="!paused"
                   large
@@ -86,10 +92,10 @@
                 <v-slider
                   v-model="currentTime"
                   class="ml-0"
-                  readonly
                   min="0"
                   :max="totalLength"
                   :label="`${nowTimerDisplay} - ${endTimerDisplay}`"
+                  @change="changeSliderTime"
                 />
               </v-card>
             </v-col>
@@ -131,7 +137,10 @@ export default {
       endTimerDisplay: null,
       getTimerNow: null,
       paused: false,
+      sliderChange: false,
+      randomText: 'ndeio wendiowe nfie fwe',
       logs: [],
+      frames: [],
       cols: 0,
       rows: 0,
     };
@@ -174,6 +183,9 @@ export default {
       this.getTimerNow = this.getDisplaySliderInfo(this.currentTime).display;
       this.cols = this.logs[0].width;
       this.rows = this.logs[0].height;
+      this.frames = this.createFrames();
+      // eslint-disable-next-line no-console
+      console.log(this.frames[0].incTime);
     }
   },
 
@@ -203,9 +215,30 @@ export default {
         trim: false,
       });
       return {
-        display: duration,
+        display: duration, // format to slider label
         intervalLength: interval, // length of slider in Ms
       };
+    },
+
+    createFrames() { // create cumulative frames for the exibition in slider
+      let message = '';
+      let time = 0;
+      const arrFrames = [{
+        incMessage: message,
+        incTime: time,
+      }];
+      for (let i = 0; i < this.logs.length - 1; i += 1) {
+        const future = new Date(this.logs[i + 1].time);
+        const now = new Date(this.logs[i].time);
+        const interval = future - now;
+        message += this.logs[i].message;
+        time += interval;
+        arrFrames.push({
+          incMessage: message,
+          incTime: moment.duration(time, 'milliseconds').asMilliseconds(),
+        });
+      }
+      return arrFrames;
     },
 
     timer() { // Increments the slider
@@ -222,11 +255,16 @@ export default {
       this.$nextTick(() => this.fitAddon.fit());
       this.fitAddon.fit();
       this.xterm.focus();
-      this.print(0, this.logs);
+      // this.print(0, this.logs);
+      this.printTest();
       this.timer();
       if (this.xterm.element) { // check already existence
         this.xterm.reset();
       }
+    },
+
+    changeSliderTime() { // Moving the Slider
+      this.sliderChange = true;
     },
 
     close() {
@@ -238,8 +276,14 @@ export default {
       this.paused = false;
     },
 
+    printTest() { // delete
+      this.xterm.write(this.frames[this.frames.length - 1].incMessage);
+      setTimeout(() => { this.xterm.write('\u001Bc'); this.xterm.write('Another Random Message\n'); }, 4000);
+    },
+
     print(i, logsArray) {
       if (!this.paused) {
+        this.sliderChange = false;
         this.xterm.write(`${logsArray[i].message}`);
         if (i === logsArray.length - 1) return;
         const nowTimerDisplay = new Date(logsArray[i].time);
